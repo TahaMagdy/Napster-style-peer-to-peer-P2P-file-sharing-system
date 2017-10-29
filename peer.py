bc57b67335ce46a2db61fbb4fc01ec59f2d6ea41
TahaMagdy <tahamagdy314@gmail.com>
2017-10-29 23:47:37 +0200

    prepare peer as a client/server

--- peer.py
+++ peer.py
@@ -2,7 +2,20 @@ import rpyc
 import constants
 
 
+# Peer as a Client
 class Peer:
 
     # Connect to the server
     conn = rpyc.connect(constants.SERVER, constants.PORT)
+
+
+# Peer as a Server
+# Other peers can connect to
+class DBList(rpyc.Service):
+
+    def exposed_obtain(self, file_name):
+        '''
+            * Other peers invoke this method
+              to download file_name from this peer
+        '''
+        pass
