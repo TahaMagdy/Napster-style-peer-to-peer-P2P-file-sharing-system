f97709c1559d60f60913a74680e0d82ddc225005
TahaMagdy <tahamagdy314@gmail.com>
2017-10-29 23:12:52 +0200

    add server search & registry methods

--- central_server.py
+++ central_server.py
@@ -5,8 +5,13 @@ from rpyc.utils.server import ThreadedServer
 
 class DBList(rpyc.Service):
 
+    def exposed_registry(self, peer_id, file_name):
+        pass
+
+    def exposed_search(self, file_name):
+        pass
 
 if __name__ == "__main__":
-    server = ThreadedServer(DBList, hostname = constants.SERVER, port = constants.PORT)
+    server = ThreadedServer(DBList, hostname=constants.SERVER, port=constants.PORT)
     print("Server starts")
     server.start()
